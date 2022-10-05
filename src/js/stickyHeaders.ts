f7e158b13dbe33204f341a0467f111909fc011ec
yoyurec <yoyurec@gmail.com>
2022-10-05 21:07:17 +0300

    feat(listener): add new headers added on edit, add route change, fix bugs

--- src/js/stickyHeaders.ts
+++ src/js/stickyHeaders.ts
@@ -1,7 +1,9 @@
 import '@logseq/libs';
-import styles from '../css/stickyHeaders.css';
+import { logseq as PL } from '../../package.json';
 
-const headersWrapperSelector = `#main-content-container .page-blocks-inner .ls-block:not([haschild='']):not([data-collapsed]):not([data-refs-self='["quote"]']):not([data-refs-self='["card"]']):not(.pre-block)`;
+import '../css/stickyHeaders.css';
+
+const headersWrapperSelector = `#main-content-container .page-blocks-inner .ls-block:not([haschild='']):not([data-refs-self='["quote"]']):not([data-refs-self='["card"]']):not(.pre-block)`;
 const headersSelector = ':is(h1, h2, h3, h4, h5)';
 
 let doc: Document;
@@ -12,43 +14,36 @@ let intersectionTop: number;
 let headersMutationObserver: MutationObserver, headersMutationObserverConfig: MutationObserverInit;
 let headersIntersectObserver: IntersectionObserver, headersIntersectObserverConfig: IntersectionObserverInit;
 
-const initHeadersMutationObserver = () => {
+const initHeadersMutationObserver = async () => {
     headersMutationObserverConfig = { childList: true, subtree: true };
     headersMutationObserver = new MutationObserver(headersMutationCallback);
 }
+
 const headersMutationCallback: MutationCallback = function (mutationsList) {
     for (let i = 0; i < mutationsList.length; i++) {
         const addedNode = mutationsList[i].addedNodes[0] as HTMLElement;
-        const removedNode = mutationsList[i].removedNodes[0] as HTMLElement;
-        if (addedNode && addedNode.childNodes.length) {
-            const header = addedNode.querySelector(headersSelector);
-            if (header) {
-                console.log('header added');
-                const block = header.closest(`.ls-block:not([haschild=''])`);
-                if (block) {
+        if (addedNode && addedNode.nodeType !== Node.TEXT_NODE && addedNode.classList.contains('flex') && addedNode.classList.contains('flex-row')) {
+            const block = addedNode.closest(`.ls-block`)?.closest(`.ls-block:not([haschild=''])`);
+            if (block && !block.classList.contains('will-stick')) {
+                const headerInside = (block.firstChild as Element)?.querySelector(headersSelector);
+                if (headerInside) {
                     initHeaders([block]);
                 }
             }
         }
-        if (
-            (addedNode && addedNode.classList && addedNode.classList.contains('lsp-iframe-sandbox-container'))
-            || (removedNode && removedNode.classList && removedNode.classList.contains('lsp-iframe-sandbox-container'))
-        ) {
-            console.log(`StickyHeaders: plugin status change detected`);
-            setTimeout(() => {
-                reLoad();
-            }, 5000);
-        }
     }
 }
+
 const runHeadersMutationObserver = () => {
     if (!mainContentContainer) {
         return;
     }
-    headersMutationObserver.observe(doc.body, headersMutationObserverConfig);
+    headersMutationObserver.observe(mainContentContainer, headersMutationObserverConfig);
 }
 
 const initHeadersIntersectObserver = () => {
+    calcDimensions();
+
     headersIntersectObserverConfig = {
         root: mainContentContainer,
         rootMargin: `${intersectionTop}px 0px 0px 0px`,
@@ -68,58 +63,38 @@ const headersIntersectCallback: IntersectionObserverCallback = (entryList) => {
     }
 }
 
-const initHeaders = (blocksList: NodeListOf<Element> | Element[]) => {
+const initHeaders = (blocksList?: NodeListOf<Element> | Element[]) => {
+    if (!blocksList) {
+        blocksList = doc.querySelectorAll(headersWrapperSelector);
+    }
+    if (!blocksList) {
+        return;
+    }
     for (let i = 0; i < blocksList.length; i++) {
         const blockItem = blocksList[i] as HTMLElement;
         if (blockItem) {
-            const headerHolder = blockItem.firstChild as Element;
-            if (headerHolder.querySelector(headersSelector)) {
+            const headerWrapper = blockItem.firstChild as Element;
+            if (headerWrapper.querySelector(headersSelector)) {
                 blockItem.classList.add('will-stick');
-                headersIntersectObserver.observe(headerHolder);
+                headersIntersectObserver.observe(headerWrapper);
             }
         }
-
     }
 }
 
 const uninitHeaders = () => {
-    const headersList = doc.querySelectorAll('.will-stick');
-    for (let i = 0; i < headersList.length; i++) {
-        const headerItem = headersList[i] as HTMLElement;
-        headerItem.classList.remove('will-stick');
-        headerItem.classList.remove('is-sticky');
+    headersIntersectObserver.disconnect();
+    const blocksList = doc.querySelectorAll('.will-stick');
+    for (let i = 0; i < blocksList.length; i++) {
+        const blockItem = blocksList[i] as HTMLElement;
+        const headerWrapper = blockItem.firstChild as Element;
+        blockItem.classList.remove('will-stick');
+        headerWrapper.classList.remove('is-sticky');
     }
-}
-
-const headersLoad = () => {
-    setTimeout(() => {
-        const headersList = doc.querySelectorAll(headersWrapperSelector);
-        calcDimensions();
-        initHeadersIntersectObserver();
-        initHeaders(headersList);
-        runHeadersMutationObserver();
-    }, 5000);
 }
 
 const headersUnload = () => {
     headersMutationObserver.disconnect();
-}
-
-const reLoad = () => {
-    uninitHeaders();
-    setTimeout(() => {
-        headersIntersectObserver.disconnect();
-        calcDimensions();
-        initHeadersIntersectObserver();
-        const headersList = doc.querySelectorAll(headersWrapperSelector);
-        initHeaders(headersList);
-    }, 5000);
-}
-
-const getDOMContainers = () => {
-    doc = parent.document;
-    root = doc.documentElement;
-    mainContentContainer = doc.getElementById('main-content-container');
 }
 
 const calcDimensions = () => {
@@ -127,7 +102,7 @@ const calcDimensions = () => {
     let tabsHeight = 0;
     let mainContentContainerPTop = 0;
     let compensateTop = 0;
-    if (doc.body.classList.contains('is-solext') || doc.body.classList.contains('is-awesomeUI')) {
+    if (doc.body.classList.contains('is-awesomeUI')) {
         compensateTop = doc.getElementById('head')?.getBoundingClientRect().height || 0;
     }
     if (mainContentContainer) {
@@ -142,18 +117,49 @@ const calcDimensions = () => {
     root.style.setProperty('--sticky-headers-top', `${stickyTop}px`);
 }
 
-// Main logseq on ready
-const main = async () => {
-    console.log(`StickyHeaders: plugin loaded`);
+const getDOMContainers = async () => {
+    doc = parent.document;
+    root = doc.documentElement;
+    mainContentContainer = doc.getElementById('main-content-container');
+}
 
-    logseq.provideStyle(styles);
+const registerPlugin = async () => {
+    const pluginID = PL.id;
+    if (doc.head) {
+        doc.head.insertAdjacentHTML('beforeend', `<link rel="stylesheet" id="css-awesomeLinks" href="lsp://logseq.io/${pluginID}/dist/assets/stickyHeaders.css">`)
+    }
+}
 
-    getDOMContainers();
-    initHeadersMutationObserver();
-
-    headersLoad();
-
+const runStuff = async () => {
     setTimeout(() => {
+        initHeadersIntersectObserver();
+        initHeaders();
+        initHeadersMutationObserver();
+        runHeadersMutationObserver();
+    }, 5000);
+}
+
+const reLoad = () => {
+    uninitHeaders();
+    setTimeout(() => {
+        initHeadersIntersectObserver();
+        initHeaders();
+    }, 1000);
+}
+
+const onRouteChangedCallback = () => {
+    headersMutationObserver.disconnect();
+    setTimeout(() => {
+        initHeaders();
+        runHeadersMutationObserver();
+    }, 1000);
+}
+
+const runListeners = async () => {
+    setTimeout(() => {
+        logseq.App.onRouteChanged(() => {
+            onRouteChangedCallback();
+        });
         logseq.App.onThemeChanged(() => {
             reLoad();
         });
@@ -163,8 +169,16 @@ const main = async () => {
         logseq.beforeunload(async () => {
             headersUnload();
         });
-    }, 3000)
+    }, 5000)
+}
 
+// Main logseq on ready
+const main = async () => {
+    console.log(`StickyHeaders: plugin loaded`);
+    getDOMContainers();
+    registerPlugin();
+    runStuff();
+    runListeners();
 };
 
-logseq.ready(main).catch(console.error);
+logseq.ready(main).catch(null);
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
-import { defineConfig } from "vite";
-import logseqDevPlugin from "vite-plugin-logseq";
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
+const name = 'stickyHeaders';
+
 export default defineConfig({
-  plugins: [logseqDevPlugin()],
-  // Makes HMR available for development
+  base: '',
   build: {
     sourcemap: true,
-    target: "esnext",
-    minify: "esbuild",
-  },
+    target: 'esnext',
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/${name}.js`,
+        chunkFileNames: `assets/chunks/${name}.js`,
+        assetFileNames: `assets/${name}.[ext]`,
+      }
+    }
+  }
 });
